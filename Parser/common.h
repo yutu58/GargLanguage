bf499b0d650a337a45d3813cd5740b4c991a832e
Mattheo de Wit <mattheodw@gmail.com>
2021-01-12 11:43:56 -0500

    Create common.h

---
+++ Parser/common.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+using namespace std;
+
+extern string data;
+extern string text;
+extern string globals;
+extern string externs;
+extern string prologue;
+extern string mn;
+extern string epilogue;
+extern string subs;
+
+extern void defInt();
+
+#endif
